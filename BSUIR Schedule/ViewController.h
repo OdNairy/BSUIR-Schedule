ad46acb1edc2f6eba74db0f5dd8ff97c394d09be
Roman Gardukevich <r.gardukevich@itransition.com>
2012-09-24 11:54:56 +0300

    Initial Commit

---
+++ BSUIR Schedule/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BSUIR Schedule
+//
+//  Created by Roman Gardukevich on 24.09.12.
+//  Copyright (c) 2012 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BSUIR Schedule/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BSUIR Schedule
+//
+//  Created by Roman Gardukevich on 24.09.12.
+//  Copyright (c) 2012 Roman Gardukevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
